23ebf6ef885a9f97b734aea8636dc4b7ef312597
Christine Banek <cbanek@gmail.com>
2018-12-07 17:48:35 -0700

    [DM-16383] Rename mysql -> tap_schema.
    Since we'll have two mysqls we should name it for what it is.

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ cp build/libs/tap##1000.war docker
 (cd docker && docker build . -t lsstdax/lsst-tap-demo:latest -f Dockerfile)
 (cd docker && docker build . -t lsstdax/oracle-db-demo:latest -f Dockerfile.oracle)
 (cd docker && docker build . -t lsstdax/postgresql-db-demo:latest -f Dockerfile.postgresql)
-(cd docker && docker build . -t lsstdax/mysql-db-demo:latest -f Dockerfile.mysql)
+(cd docker && docker build . -t lsstdax/tap-schema-db:latest -f Dockerfile.tap_schema)
 (cd docker && docker build . -t lsstdax/presto:latest -f Dockerfile.presto)
--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@
 
 docker push lsstdax/oracle-db-demo:latest
 docker push lsstdax/postgresql-db-demo:latest
-docker push lsstdax/mysql-db-demo:latest
+docker push lsstdax/tap-schema-db:latest
 docker push lsstdax/lsst-tap-demo:latest
 docker push lsstdax/presto:latest
