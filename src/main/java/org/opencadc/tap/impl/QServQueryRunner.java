729b187f8334aac55384abf49f061fba540fdbab
Christine Banek <cbanek@lsst.org>
2018-12-13 16:43:54 -0700

    [DM-16711] Play with tomcat connectionpool settings and commits.
    Because there seem to be hanging transacations that are timing out,
    I'm changing a few parameters.
    
    1) Adjust pool to have more than 1 connection in it.  This will help
    (and be required) because if there's a longer running query we might
    run out of connections the way it was previously configured.
    
    2) Presto complains about transactions timing out, and I noticed some
    interesting setting of autocommit in QServQueryRunner, which I've
    commented out and just forced to true in the context.xml.
    
    With these settings I no longer see the error message in the
    original ticket at all, but I'm not sure I haven't broken something
    else.  I will be contacting CADC as to why they are playing with
    autocommit like this.

--- src/main/java/org/opencadc/tap/impl/QServQueryRunner.java
+++ src/main/java/org/opencadc/tap/impl/QServQueryRunner.java
@@ -370,7 +370,11 @@ public class QServQueryRunner implements JobRunner
                     // manually control transaction, make fetch size (client batch size) small,
                     // and restrict to forward only so that client memory usage is minimal since
                     // we are only interested in reading the ResultSet once
+
+                    /* [DM-16711] Stop messing with the autocommit value in this layer,
+                     * use the context.xml to set it overall to true in tomcat.
                     connection.setAutoCommit(false);
+                     */
                     pstmt = connection.prepareStatement(sql);
                     pstmt.setFetchSize(1000);
                     pstmt.setFetchDirection(ResultSet.FETCH_FORWARD);
@@ -435,11 +439,15 @@ public class QServQueryRunner implements JobRunner
             {
                 if (connection != null)
                 {
+/*
+ * [DM-16711] Stop messing with the autocommit value in this layer,
+ * use the context.xml to set it overall to true in tomcat.
                     try
                     {
                         connection.setAutoCommit(true);
                     }
                     catch(Throwable ignore) { }
+*/
                     try
                     {
                         resultSet.close();
