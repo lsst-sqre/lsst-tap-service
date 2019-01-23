6d7310de6f168d432bee6a6f5a58caca1a88645b
Christine Banek <cbanek@lsst.org>
2019-01-23 16:18:37 -0700

    [DM-17380] Add support for slack webhook notifications.

--- integration_test/querymonkey/querymonkey.py
+++ integration_test/querymonkey/querymonkey.py
@@ -23,6 +23,18 @@ def generate_parameters():
         'r4': random.uniform(0, 1)
     }
 
+def send_notification(error_count, query, error):
+    ERROR_LIMIT = 3
+    msg = '[%i] errors in a row. Running [%s] threw [%s]' \
+        % (error_count, query, error)
+    logging.error(msg)
+
+    if args.slack_webhook and error_count <= ERROR_LIMIT:
+        if error_count == ERROR_LIMIT:
+            msg = '[%i] errors in a row, squelching' % ERROR_LIMIT
+        r = requests.post(args.slack_webhook, json={'text': msg})
+        logging.info('Slack webook post returns: %i', r.status_code)
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -31,12 +43,15 @@ parser.add_argument('--server', required=True)
 parser.add_argument('--timer', type=int, default=os.environ.get('timer', 60))
 parser.add_argument('--dir', default='test_queries')
 parser.add_argument('--dry-run', action='store_true')
+parser.add_argument('--slack-webhook')
 args = parser.parse_args()
 
 query_templates = os.listdir(args.dir)
 service = pyvo.dal.TAPService(args.server)
 
 query_id = 0
+success_id = 0
+
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(args.dir),
     undefined=jinja2.StrictUndefined
@@ -54,8 +69,13 @@ while(True):
     logging.info("[%i] Running: %s", query_id, query)
     start = time.time()
 
-    if not args.dry_run:
-        service.search(query)
+    try:
+        if not args.dry_run:
+            service.search(query)
+        success_id = query_id
+    except Exception as e:
+        error_count = query_id - success_id
+        send_notification(error_count, query, str(e))
 
     end = time.time()
     logging.info("[%i] Finished, took: %i seconds", query_id, end - start)
