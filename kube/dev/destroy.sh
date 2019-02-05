3f145682823fabdfcbe1e6c4ff234127f5c7a09a
Christine Banek <cbanek@lsst.org>
2019-02-05 13:29:43 -0700

    [DM-17565] Cleanup dev deployment.
    Get rid of NCSA options, get rid of the special ingress rule,
    and the querymonkey since there's no data to query.

--- kube/dev/destroy.sh
+++ kube/dev/destroy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -x
-# If the hostname we're deploying from is at NCSA,
-# this string will not be empty, allowing for us to
-# do different things for GKE / NCSA.
-NCSA_DEPLOY=`hostname -f | grep ncsa`
 
 # Delete the Oracle backend.
 kubectl delete deployment/oracle-deployment
@@ -12,11 +8,9 @@ kubectl delete service/oracle-service
 kubectl delete deployment/tap-schema-deployment
 kubectl delete service/tap-schema-service
 
-if [ -z "$NCSA_DEPLOY" ]; then
-  # Delete the mock qserv backend.
-  kubectl delete deployment/mock-qserv-deployment
-  kubectl delete service/qserv-master01
-fi
+# Delete the mock qserv backend.
+kubectl delete deployment/mock-qserv-deployment
+kubectl delete service/qserv-master01
 
 # Delete the postgres backend for UWS jobs.
 kubectl delete service/postgresql-service
@@ -28,4 +22,4 @@ kubectl delete service/tap-service
 kubectl delete ingress/tap-ingress
 
 # Delete the presto deployment.
-helm delete --purge dax
+helm delete --purge dax-dev
