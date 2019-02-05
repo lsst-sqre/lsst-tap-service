0cd272cc5d69f3ebe3bbdffc549d42e0a0321226
Christine Banek <cbanek@lsst.org>
2019-02-05 16:08:00 -0700

    [DM-17558] Add helper scripts for monitoring
    Add a monitor.sh to begin monitoring, and destroy.sh cleans up
    the monitoring.  Keep this as a separate script so the monitoring
    can be started after the cluster has started up.

---
+++ kube/stable/monitor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+# Start the query monkey to monitor the deployment.
+# This assumes it is being run at NCSA.
+DAX_NAMESPACE=${DAX_NAMESPACE:-'dax-stable'}
+
+kubectl create -f querymonkey-deployment.yaml --namespace $DAX_NAMESPACE
