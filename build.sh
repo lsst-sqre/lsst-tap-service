6d284b66fc4f889a77c46ec07b036aaa1b2e7587
Christine Banek <cbanek@lsst.org>
2019-03-08 14:55:07 -0700

    [DM-17187] Get rid of monkeypatch, go back to mainline cadc-adql.
    Bump up the dependency version of cadc-adql (which is part of the
    https://github.com/opencadc/tap repo) to 1.1.4, which includes
    my PR to add support for the DISTANCE function.  We need this
    to have the DISTANCE parsing call back into the QServ specific
    region converter.
    
    Since we're going to 1.1.4, which has some of pdowler's changes
    for catalog.schema.table names, we can stop doing the monkeypatch,
    so get rid of all that, and re-enable testing during a build.
    
    The tests used to fail because of the catalog.schema.table change.
    
    So overall, pay off some debt!

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
-gradle --info clean build -x test
+gradle --info clean build
 cp build/libs/tap##1000.war docker
-./monkeypatch_tapwar.sh
 (cd docker && docker build . -t lsstdax/lsst-tap-demo:latest -f Dockerfile)
 (cd docker && docker build . -t lsstdax/oracle-db-demo:latest -f Dockerfile.oracle)
 (cd docker && docker build . -t lsstdax/postgresql-db-demo:latest -f Dockerfile.postgresql)
