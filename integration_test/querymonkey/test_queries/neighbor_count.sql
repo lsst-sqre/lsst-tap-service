48eb4dad262e2f482c2413ffab6cd6317e5f77d4
Christine Banek <cbanek@lsst.org>
2019-05-08 16:02:00 -0700

    [DM-19688] Remove DISTANCE from neighbor_count

--- integration_test/querymonkey/test_queries/neighbor_count.sql
+++ integration_test/querymonkey/test_queries/neighbor_count.sql
@@ -1,5 +1,4 @@
-SELECT 'monkey', count(o1.id1),
-  DISTANCE(POINT('ICRS', o1.coord_ra, o1.coord_decl), POINT('ICRS', o2.coord_ra, o2.coord_decl)) AS d
+SELECT 'monkey', count(o1.id)
 FROM sdss_stripe82_01.RunDeepSource o1, sdss_stripe82_01.RunDeepSource o2
 WHERE CONTAINS(POINT('ICRS', o1.coord_ra, o1.coord_decl), CIRCLE('ICRS', {{ ra }}, {{ dec }}, {{ r1 }})) = 1
   AND DISTANCE(POINT('ICRS', o1.coord_ra, o1.coord_decl), POINT('ICRS', o2.coord_ra, o2.coord_decl)) < 0.016
