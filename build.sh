2b7a5079b8cc0c53ce1175a44e3bb38864e35ebd
Christine Banek <cbanek@lsst.org>
2019-10-30 14:53:57 -0700

    [DM-22013] Cleanup travis testing
    Clean up the travis testing.  Get more consistent about names of
    images throughout, and using the images we just built instead of
    testing images that we won't push.  We build the dev tagged images
    locally, and then use docker-compose to test those.
    
    Get rid of the unused docker-compose-no-db.yml file.
    
    Make it so you can't push the dev tag, which might cause confusion.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-gradle --info clean build
-cp build/libs/tap##1000.war docker
+gradle --stacktrace --info clean assemble javadoc build test
+cp build/libs/*.war docker
 (cd docker && docker build . -t lsstdax/lsst-tap-service:dev -f Dockerfile.lsst-tap-service)
 (cd docker && docker build . -t lsstdax/uws-db:dev -f Dockerfile.uws-db)
 (cd docker && docker build . -t lsstdax/tap-schema-db:dev -f Dockerfile.tap-schema-db)
--- push.sh
+++ push.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 TAG=${1:?'You must specify the image tag to push'}
 
+if [ $TAG == "dev" ]; then
+  echo "Don't try to push the dev tag.  Dev is only for local testing."
+  exit 1
+fi
+
 docker tag lsstdax/lsst-tap-service:dev lsstdax/lsst-tap-service:$TAG
 docker tag lsstdax/uws-db:dev lsstdax/uws-db:$TAG
 docker tag lsstdax/tap-schema-db:dev lsstdax/tap-schema-db:$TAG
