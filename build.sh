9040b1100b2ec3053ffd47dd292fd6c9f3863add
Christine Banek <cbanek@gmail.com>
2021-06-22 17:24:01 -0700

    [DM-30753] Get rid of tap-schema build parts
    Just rely on the tap-schema builds from sdm_schemas

--- build.sh
+++ build.sh
@@ -7,7 +7,3 @@ cp build/libs/*.war docker
 (cd docker && docker build . -t lsstdax/uws-db:dev -f Dockerfile.uws-db)
 (cd docker && docker build . -t lsstdax/mock-qserv:dev -f Dockerfile.mock-qserv)
 docker build . -t lsstdax/querymonkey:dev -f docker/Dockerfile.querymonkey
-
-# Build an example TAP_SCHEMA database that is empty other
-# than required schema.
-(cd tap-schema && docker build . -t lsstdax/tap-schema-db:dev -f Dockerfile)
--- test_minikube.sh
+++ test_minikube.sh
@@ -6,16 +6,7 @@ else
   CHART=lsst-sqre/cadc-tap
 fi
 
-if [ -f tap-schema-dev-chart.tgz ]
-then
-  TAP_SCHEMA_CHART=tap-schema-dev-chart.tgz
-else
-  TAP_SCHEMA_CHART=lsst-sqre/tap-schema
-fi
-
 helm delete tap-dev -n tap-dev || true
-helm delete tap-schema -n tap-schema || true
 
 ./build.sh
 helm upgrade --install tap-dev $CHART --create-namespace --namespace tap-dev --values dev-values.yaml
-helm upgrade --install tap-schema $TAP_SCHEMA_CHART --create-namespace --namespace tap-schema --values tap-schema-values.yaml
