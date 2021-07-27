984b7eba0617f47347727e0a7b58dc113b03a011
Christine Banek <cbanek@gmail.com>
2021-07-26 18:16:17 -0700

    [DM-31210] Clean up reaper code a bit
    Get rid of some spammy logs and only reference bucket name once.

--- tools/async_reaper.py
+++ tools/async_reaper.py
@@ -4,15 +4,15 @@ from google.cloud import storage
 
 storage_client = storage.Client()
 
-bucket = storage_client.bucket("async-results.lsst.codes")
+bucket_name = "async-results.lsst.codes"
 
-files = storage_client.list_blobs("async-results.lsst.codes")
+bucket = storage_client.bucket(bucket_name)
+
+files = storage_client.list_blobs(bucket_name)
 
 now = datetime.now(timezone.utc)
 
 for f in files:
-    print(f"{f.name} created on {f.updated} with size {f.size}")
-
     age = now - f.updated
     if age.days > 30:
         print(f"Deleting {f.name} because it's too old.")
