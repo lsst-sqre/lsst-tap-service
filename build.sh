aa970c771ba12027487cde6c62a969ff0ee370ed
Christine Banek <cbanek@gmail.com>
2021-10-11 14:15:55 -0700

    [DM-31965] Add in patched cadc-adql
    So this PR hasn't been accepted yet upstream, but we have one that
    fixes some of the parsing and type coercian for math in sql.  Until
    then we should pin this version that I've checked in and built myself
    with the PR branch.
    
    https://github.com/opencadc/tap/pull/116

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # Build the TAP service and docker images.
 gradle --stacktrace --info clean assemble javadoc build test
 cp build/libs/*.war docker
+
+# Apply some patches and upstream dependencies that
+# haven't been accepted yet.
+jar -uf docker/tap##1000.war -C docker/patches WEB-INF/lib/cadc-adql-1.1.10.jar
+
 (cd docker && docker build . -t lsstdax/lsst-tap-service:dev -f Dockerfile.lsst-tap-service)
 (cd docker && docker build . -t lsstdax/uws-db:dev -f Dockerfile.uws-db)
 (cd docker && docker build . -t lsstdax/mock-qserv:dev -f Dockerfile.mock-qserv)
