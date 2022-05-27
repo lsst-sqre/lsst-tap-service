ff318310c06d36dee7ad946cbcd369fbe6eb98ac
Christine Banek <cbanek@gmail.com>
2022-05-27 13:25:41 -0700

    [DM-34891] Try to hold back vosi requirement
    The tap example holds this back from a major version change, which
    is maybe where our exception problems are coming from.
    
    Current problem:
    
    Receiver class ca.nrc.cadc.dali.tables.votable.VOTableWriter$TabledataMaxIterations does not define or inherit an implementation of the resolved method 'abstract void maxIterationsReached(boolean)' of interface ca.nrc.cadc.xml.MaxIterations.
    
    Other commit:
    
    https://github.com/opencadc/tap/commit/3023c9ed7a034800b6390da37cded35f72c49aef
    
    Also since the dali vosi change has been merged, we can get rid of this patched version:
    https://github.com/opencadc/dal/pull/70

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ cp build/libs/*.war docker
 # Apply some patches and upstream dependencies that
 # haven't been accepted yet.
 jar -uf docker/tap##1000.war -C docker/patches WEB-INF/lib/cadc-adql-1.1.10.jar
-jar -uf docker/tap##1000.war -C docker/patches WEB-INF/lib/cadc-dali-1.2.11.jar
 
 (cd docker && docker build . -t lsstdax/lsst-tap-service:dev -f Dockerfile.lsst-tap-service)
 (cd docker && docker build . -t lsstdax/uws-db:dev -f Dockerfile.uws-db)
