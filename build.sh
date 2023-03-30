331c330d906e83d2f6d79fae84250a84b546c60d
Christine Banek <cbanek@lsst.org>
2023-03-30 14:58:56 -0700

    [DM-38493] Update cadc-adql patch
    Build from https://github.com/cbanek/tap/tree/tickets/DM-17872

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cp build/libs/*.war docker
 
 # Apply some patches and upstream dependencies that
 # haven't been accepted yet.
-jar -uf docker/tap##1000.war -C docker/patches WEB-INF/lib/cadc-adql-1.1.10.jar
+jar -uf docker/tap##1000.war -C docker/patches WEB-INF/lib/cadc-adql-1.1.13.jar
 
 docker build . -t lsstdax/lsst-tap-service:dev -f docker/Dockerfile.lsst-tap-service
 docker build . -t lsstdax/uws-db:dev -f docker/Dockerfile.uws-db
