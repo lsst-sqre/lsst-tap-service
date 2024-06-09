38f75b52040f3bdc39b215cae5a6f3e93dc35b97
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2024-06-08 20:08:41 -0700

    Update cadc libs / Enable UWSInitAction / Deprecate Authenticator / Update Docker container / Fix maxActive setting

--- docker/start.sh
+++ docker/start.sh
@@ -6,5 +6,3 @@ if [ -n "$DATALINK_PAYLOAD_URL" ]; then
     curl -L "$DATALINK_PAYLOAD_URL" -o /tmp/datalink_payload.zip
     unzip /tmp/datalink_payload.zip -d /tmp/datalink
 fi
-
-exec catalina.sh run
