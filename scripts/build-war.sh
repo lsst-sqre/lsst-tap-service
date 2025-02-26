a7871d43efb84c7d1013ee8e20acc0b01d60a5ab
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2025-02-26 05:44:06 -0700

    Remove cadc-adql 1.1.13 patch from build

--- scripts/build-war.sh
+++ scripts/build-war.sh
@@ -7,6 +7,3 @@ set -exo pipefail
 # Build the TAP service and docker images.
 gradle --stacktrace --info clean assemble javadoc build test
 cp build/libs/*.war docker
-
-# Apply some patches and upstream dependencies that haven't been accepted yet.
-jar -uf docker/tap##1000.war -C docker/patches WEB-INF/lib/cadc-adql-1.1.13.jar
