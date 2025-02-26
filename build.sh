8ffd8706e1d9dd24a257906dce5eacac61966e49
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2025-02-26 05:14:27 -0700

    Set cadc-adql requirement to 1.1.14 and above and remove previous dependency

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 
 # Build the TAP service and docker images.
 gradle --stacktrace --info clean assemble javadoc build test
-cp build/libs/*.war docker
 
-# Apply some patches and upstream dependencies that
-# haven't been accepted yet.
-jar -uf docker/tap##1000.war -C docker/patches WEB-INF/lib/cadc-adql-1.1.13.jar
+cp build/libs/*.war docker
 
 docker build . -t lsstdax/lsst-tap-service:dev -f docker/Dockerfile.lsst-tap-service
 docker build . -t lsstdax/uws-db:dev -f docker/Dockerfile.uws-db
